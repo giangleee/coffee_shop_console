78bd31a40154b54fd07f04f669761f1ae6250448
giangleee <giangle602@gmail.com>
2022-01-09 20:28:48 +0700

    built base

---
+++ source/layouts/AuthPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <HeaderPage />
+    <Nuxt />
+    <FooterPage />
+  </div>
+</template>
+
+<style>
+  @import '../style/layouts/_main.scss';
+</style>
---
+++ source/pages/faq.vue
@@ -0,0 +1,177 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="faq-wrapper">
+                    <div class="header">
+                        <h1>FAQ - frequently asked question</h1>
+                    </div>
+
+                    <!--Part 1-->
+                    <div class="fqa-inner">
+                        <div class="fqa-item">
+                            <div class="fqa-title">
+                                <label> 1. FAQ about Company review </label>
+                                <button
+                                    href="#info1"
+                                    class="btn-plus"
+                                    data-bs-toggle="collapse"
+                                >
+                                    <i class="fas fa-plus 2x faq-plus"></i>
+                                </button>
+                            </div>
+
+                            <div id="info1" class="content collapse">
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make a type specimen book. It has
+                                    survived not only five centuries, but also
+                                    the leap into electronic typesetting,
+                                    remaining essentially unchanged.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--Part 2-->
+                    <div class="fqa-inner">
+                        <div class="fqa-item">
+                            <div class="fqa-title">
+                                <label> 2. FAQ about CV Review? </label>
+                                <button
+                                    href="#info2"
+                                    class="btn-plus"
+                                    data-bs-toggle="collapse"
+                                >
+                                    <i class="fas fa-plus 2x faq-plus"></i>
+                                </button>
+                            </div>
+
+                            <div id="info2" class="content collapse">
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make a type specimen book. It has
+                                    survived not only five centuries, but also
+                                    the leap into electronic typesetting,
+                                    remaining essentially unchanged.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="fqa-inner">
+                        <div class="fqa-item">
+                            <div class="fqa-title">
+                                <label>
+                                    3. FAQ about Al Match for candidate?
+                                </label>
+                                <button
+                                    href="#info3"
+                                    class="btn-plus"
+                                    data-bs-toggle="collapse"
+                                >
+                                    <i class="fas fa-plus 2x faq-plus"></i>
+                                </button>
+                            </div>
+
+                            <div id="info3" class="content collapse">
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make a type specimen book. It has
+                                    survived not only five centuries, but also
+                                    the leap into electronic typesetting,
+                                    remaining essentially unchanged.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="fqa-inner">
+                        <div class="fqa-item">
+                            <div class="fqa-title">
+                                <label>
+                                    4. FAQ about Viet Nam Best IT Companies?
+                                </label>
+                                <button
+                                    href="#info4"
+                                    class="btn-plus"
+                                    data-bs-toggle="collapse"
+                                >
+                                    <i class="fas fa-plus 2x faq-plus"></i>
+                                </button>
+                            </div>
+
+                            <div id="info4" class="content collapse">
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make a type specimen book. It has
+                                    survived not only five centuries, but also
+                                    the leap into electronic typesetting,
+                                    remaining essentially unchanged.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="fqa-inner">
+                        <div class="fqa-item">
+                            <div class="fqa-title">
+                                <label> 5. What is status of residence? </label>
+                                <button
+                                    href="#info5"
+                                    class="btn-plus"
+                                    data-bs-toggle="collapse"
+                                >
+                                    <i class="fas fa-plus 2x faq-plus"></i>
+                                </button>
+                            </div>
+
+                            <div id="info5" class="content collapse">
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make a type specimen book. It has
+                                    survived not only five centuries, but also
+                                    the leap into electronic typesetting,
+                                    remaining essentially unchanged.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default {
+    layout: 'AuthPage',
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '../style/pages/faq.scss';
+</style>
